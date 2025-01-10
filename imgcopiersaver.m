779c15b92a70fb10a483d5c4040b2e0725567b5f
cbphoto720 <cbphoto720@gmail.com>
2025-01-10 09:35:19 -0800

    Auto-detect .utc filename, accept matrix as input for numCopies

--- imgcopiersaver.m
+++ imgcopiersaver.m
@@ -1,44 +1,63 @@
-function imgcopiersaver(inputFolder, outputFolder, numCopies, baseFilename, camSN)
+function imgcopiersaver(inputFolder, outputFolder, numCopies, camSN)
     % PROCESS_TIF_IMAGES Processes TIFF images by copying and renaming them.
     %
     % Args:
     %   inputFolder (string): Path to the folder containing subfolders with .tif images.
     %   outputFolder (string): Path to the folder where processed images will be saved.
     %   numCopies (double): Number of copies to create for each image.
-    %   baseFilename (string): Common base filename for all processed images.
     %   camSN: The camera "Serial Number" is the 8 digit code included in the filename of the image e.g. _21217396_
     %% Input Parsing
     camSNdatabase=[21217396,22296748,22296760];
 
     p= inputParser;
     validScalarPosNum = @(x) isnumeric(x) && isscalar(x) && (x > 0);
-    validPosNx1Matrix = @(x) isnumeric(x) && ismatrix(x) && size(x, 2) == 1 && all(x > 0);
+    validPosNx1Matrix = @(x) isnumeric(x) && ismatrix(x) && size(x, 2) == 1 && size(x,1) > 1 && all(x > 0);
     validCamSN = @(x) any(x==camSNdatabase);
    
     addRequired(p, 'inputFolder',@(x) isfolder(x));
     addRequired(p, 'outputFolder',@(x) isfolder(x));
     addRequired(p, 'numCopies',@(x) validScalarPosNum(x) || validPosNx1Matrix(x));
+    addRequired(p, 'camSN', validCamSN);
     
-    addRequired(p, 'camSN',@(x) validCamSN);
-    
-    parse(p,inputFolder,outputFolder,numCopies, date, path);
+    parse(p,inputFolder,outputFolder,numCopies,camSN);
 
 
     %%
     % Ensure output folder exists
     if ~isfolder(outputFolder)
-        mkdir(outputFolder);
+        error('Could not find file output folder, check file path and make sure that a .utc file exists there')
     end
 
-    % Get list of all .tif files in input folder and subfolders
+    % Get list required files
     tifFiles = dir(fullfile(inputFolder, '**', '*.tif'));
+    utcFiles = dir(fullfile(outputFolder, '*.utc'));
 
     if isempty(tifFiles)
         error('No TIFF files found in the input folder or its subfolders.');
     end
 
+    if isempty(utcFiles)
+        error('No .utc files found in the folder: %s', outputFolder);
+    elseif numel(utcFiles) > 1
+        error('Multiple .utc files found in the folder: %s. There should be exactly one.', outputFolder);
+    else
+        baseFilename = utcFiles.name(1:end-4);  % Copy the name of the single .utc file found
+    end
+
+    % Generate a matrix that defines the number of copies we need for each
+    % image.  We only want 1 frame per GPS point visible in the picture, no extras
+    if validPosNx1Matrix(numCopies)
+        numberofcopiesperimg=numCopies;
+    elseif validScalarPosNum(numCopies)
+        numberofcopiesperimg=repmat(numCopies,1,length(tifFiles));
+    end
+    maxframenumber=sum(numberofcopiesperimg)-1;
+    % Determine the required number of digits for leading zero padding
+    numDigits = max(1, ceil(log10(maxframenumber + 1))); % +1 to handle exact powers of 10
+
     setnumber=0; % allocate for file naming
     filescreated=0;
+    numCopiesIND=1;
     % Process each .tif file
     for fileIdx = 1:length(tifFiles)
         % Read the full path of the current .tif file
@@ -46,9 +65,10 @@ function imgcopiersaver(inputFolder, outputFolder, numCopies, baseFilename, camS
 
         if contains(tifFiles(fileIdx).name,"_" + camSN + "_")
             % Create copies with sequential numbering
-            for copyIdx = 0:(numCopies - 1)
+            for copyIdx = 0:(numberofcopiesperimg(numCopiesIND) - 1)
                 % Generate the new filename
-                newFilename = sprintf('%s_%02d.tif', baseFilename, setnumber); %FLAG: leading zeros should be smart to account for the max # of frames
+                formatString = sprintf('%%s_%%0%dd.tif', numDigits);
+                newFilename = sprintf(formatString, baseFilename, setnumber); %numDigits dynamically adjusts the zero padding based on max frames
                 setnumber = setnumber + 1;
                 outputFile = fullfile(outputFolder, newFilename);
     
@@ -66,8 +86,9 @@ function imgcopiersaver(inputFolder, outputFolder, numCopies, baseFilename, camS
                 end
                 filescreated=filescreated+1;
             end
+            numCopiesIND=numCopiesIND+1;
         end
     end
 
-    fprintf('Processed %d TIFF files and created %d copies for each set with the correct camera serial number. (%d files created) \n', length(tifFiles), numCopies,filescreated);
+    fprintf('Looked at %d TIFF files and found the relevant CamSN. (%d files created) \n', length(tifFiles), filescreated);
 end
