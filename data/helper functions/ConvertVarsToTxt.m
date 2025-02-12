0787ec38cc60d65fb0c7936e39b8c0c7f13ce13f
cbphoto720 <cbphoto720@gmail.com>
2025-02-11 16:33:22 -0800

    Helper function to convert workspace variables to text file

---
+++ data/helper functions/ConvertVarsToTxt.m
@@ -0,0 +1,20 @@
+% Get all variables in the workspace
+vars = whos; 
+
+% Open a file to write
+fileID = fopen('workspace_variables.txt', 'w');
+
+% Loop through each variable
+for i = 1:length(vars)
+    varValue = eval(vars(i).name); % Get variable value
+    
+    % Check if numeric and matrix
+    if ismatrix(varValue) && isnumeric(varValue)
+        fprintf(fileID, '%g\t', varValue); % Print values with tabs
+    end
+end
+
+% Close file
+fclose(fileID);
+
+disp('Workspace variables saved to workspace_variables.txt');
